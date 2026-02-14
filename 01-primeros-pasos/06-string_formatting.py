eb39ef0f3c3c2c446c17e33b9dd1e30e7c65086a
Jose A Cantu <jacantu@hotmail.com>
2026-02-14 10:38:48 -0600

    add 06-string_formatting.py

---
+++ 01-primeros-pasos/06-string_formatting.py
@@ -0,0 +1,13 @@
+
+# Concatenación
+
+name = 'José'
+last_name = 'Cantú'
+age = 69
+
+# Concatenación con f (formatting)
+# full_name = name + ' ' + last_name
+full_name = f'{name} {last_name}'
+message = f'{full_name}, tienes {age}, años'
+print(full_name)
+print(message)
