c003e7bda55c892065baf881a5a7f147b5a8b2ba
Jose A Cantu <jacantu@hotmail.com>
2026-02-14 10:41:02 -0600

    add 10-math_operations.py

---
+++ 01-primeros-pasos/10-math_operations.py
@@ -0,0 +1,34 @@
+
+# Suma
+print(2 + 2)
+
+# Resta
+print(4 - 2)
+
+# Multiplicación
+print(4 * 2)
+
+# División
+print(25 / 4)
+
+# División sin decimal
+print(25 // 4)
+
+# Otras operaciones
+# Potencia
+print(2 ** 3)
+
+# Módulo: Lo que sobra de la división o Residuo
+print(6 % 4)
+
+# Redondear
+print(round(3.1))  # 3
+print(round(3.5))  # 4
+
+# 2, No redondea a 3 porque el .5 redondea al próximo numero par seigue el 3 que es impar
+print(round(2.5))
+
+print(round(2.51))  # De esta forma si redondea a 3 porque ya es mayor .51 a .5
+
+# Valor absoluto
+print(abs(-20))
