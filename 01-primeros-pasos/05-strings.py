9fb98429447655ab62c200e18120879dbda72967
Jose A Cantu <jacantu@hotmail.com>
2026-02-14 10:37:38 -0600

    add 05-strings.py

---
+++ 01-primeros-pasos/05-strings.py
@@ -0,0 +1,20 @@
+
+text1 = 'Hola'
+text2 = "José"
+
+text3 = '''
+Hola,
+soy José
+vivo en Apodaca
+'''
+print(text1)
+print(text2)
+print(text3)
+
+# Concatenación
+
+name = 'José'
+last_name = 'Cantú'
+
+full_name = name + ' ' + last_name
+print(full_name)
