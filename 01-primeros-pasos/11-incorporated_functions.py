f59e7f8e78c5447390cb08e377c1d056ed0224f0
Jose A Cantu <jacantu@hotmail.com>
2026-02-14 10:41:33 -0600

    add 11-incorporated_functions.py

---
+++ 01-primeros-pasos/11-incorporated_functions.py
@@ -0,0 +1,14 @@
+
+# Funciones incorporadas o Funciones nativas
+print("Hola Mundo")
+type(100)
+str(100)
+round(20.9)
+abs(-20)
+
+# Métodos son funciones específicas a una clase
+message = "Hola"
+message.upper()  # HOLA
+message = 'Es un buen programador'
+new_message = message.replace('Es', 'Soy')
+print(new_message)
