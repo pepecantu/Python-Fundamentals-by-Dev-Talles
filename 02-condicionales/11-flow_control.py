fcc395c21b54af2465b6b23528ebd8bb1b63fb55
Jose A Cantu <jacantu@hotmail.com>
2026-02-15 00:10:58 -0600

    add 11-flow_control.py

---
+++ 02-condicionales/11-flow_control.py
@@ -0,0 +1,16 @@
+import os
+os.system("cls")
+
+
+edad = int(input("Introduce tu edad: "))
+
+if edad < 0:
+    print("La edad no puede ser negativa")
+elif edad <= 12:
+    print("Eres un infante")
+elif edad <= 17:
+    print("Eres un adolecente")
+elif edad <= 64:
+    print("Eres un adulto")
+else:
+    print("Eres un adulto mayor")
