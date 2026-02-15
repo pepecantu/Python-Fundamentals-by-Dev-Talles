6a436c57ef4deac69dcba4f52b7164b350234831
Jose A Cantu <jacantu@hotmail.com>
2026-02-15 00:05:40 -0600

    add 01-if_conditional.py

---
+++ 02-condicionales/01-if_conditional.py
@@ -0,0 +1,17 @@
+import os
+os.system("cls")  # limpia la consola
+
+
+is_old = False
+
+if is_old:
+    print(" Puedes manejar")
+else:
+    print("Espera cumplir la mayoria de edad")
+
+is_student = True
+
+if is_student:
+    print("Obten la licencia de estudiante")
+else:
+    print("Lo siento, necesitas comprar una licencia normal")
