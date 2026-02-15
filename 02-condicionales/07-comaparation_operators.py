9c77979c60f332ea539e2c5f954dc4a6aaa3f620
Jose A Cantu <jacantu@hotmail.com>
2026-02-15 00:08:44 -0600

    add 07-comparation_operators.py

---
+++ 02-condicionales/07-comaparation_operators.py
@@ -0,0 +1,44 @@
+import os
+os. system("cls")
+
+# equal to
+print("\n Equal to operator \n")
+
+print(5 == 5)  # True
+print(5 == 6)  # False
+print("hello" == "hello")  # True
+print("hello" == "Hello")  # False
+print("\n")
+
+# operator not equal to !=
+print("\n Not equal to operator \n")
+
+print(5 != 5)  # False
+print(5 != 6)  # True
+print("hello" != "world")  # True
+print("\n")
+
+# operatosr greater than >
+print("\n Greater than operator \n")
+
+print(5 > 6)  # False
+print(5 > 4)  # True
+print("\n")
+
+# operators less than <
+print("\n Less than operator \n")
+print(5 < 6)  # True
+print(5 < 4)  # False
+
+# operators greater than or equal to >=
+print("\n Greater than or equal to operator \n")
+print(5 >= 6)  # False
+print(5 >= 5)  # True
+print(5 > 5)  # False
+print("\n")
+
+# operators less than or equal to <=
+print("\n Less than or equal to operator \n")
+print(5 <= 6)  # True
+print(5 <= 5)  # True
+print(5 < 5)  # False
