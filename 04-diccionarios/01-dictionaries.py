cb42b509fa8acaeff60bbb83dc83fcf1bf4c46eb
Jose A Cantu <jacantu@hotmail.com>
2026-02-17 04:37:11 -0600

    add 01-dictionaries.py

---
+++ 04-diccionarios/01-dictionaries.py
@@ -0,0 +1,18 @@
+import os
+os.system('cls')
+
+dictionary = {
+    "key": "value",
+    "a": [1, 2, 3],
+    "b": 2,
+    "verdad": True
+}
+
+print(dictionary["verdad"])
+
+user = {
+    "name": "José Angel",
+    "country": "México",
+    "age": 69
+}
+print(user["name"])
